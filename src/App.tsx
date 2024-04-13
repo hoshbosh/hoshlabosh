e435587c4e94713942a396efa8e961425b751562
hoshbosh <jlabasbas24@gmail.com>
2024-04-13 16:08:45 -0400

    init

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+import './index.css'
+
+function App() {
+
+  return (
+      <div className='primary-container'>
+        <div className='section'>
+          <div className='title text-center'>
+            Boo
+          </div>
+        </div>
+        <div className='section bg-charcoal box bg-gradient-to-t to-moonstone from-powderBlue '>
+          <div className='title text-charcoal'>
+            Yuh ay
+          </div>
+          <div className='text text-charcoal'>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestiae quidem sapiente dolorem aspernatur? Quia in corporis perspiciatis blanditiis nam sit soluta dignissimos, beatae necessitatibus, voluptatum odio totam quisquam libero quam?
+          </div>
+        </div>
+      </div>
+  )
+}
+
+export default App
---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{tsx,js, jsx, ts}"
+  ],
+  theme: {
+    colors:{
+      "plat":"#D8DBE2",
+      "powderBlue":"#A9BCD0",
+      "moonstone":"#58A4B0",
+      "charcoal":"#373F51",
+      "eerieBlack":"#1B1B1E",
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
